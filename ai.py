e1e1c12118ace3e843218da93a82bda12ce2fcab
kievkun <kievkunni@gmail.com>
2024-09-05 11:59:29 +0900

    v0.0.1 test push

---
+++ ai.py
@@ -0,0 +1,9 @@
+
+for i in range(1, 10) :
+    for k in range(1, 10):
+        if (k < 9):
+            print(i, "x", k, "=", i * k)
+        else:
+            print(f"{i} x {k} = {i * k}.")
+
+
---
+++ tesyt.py
@@ -0,0 +1,10 @@
+print("hello world")
+a = 5
+i = 0
+for i in range(1, 11):
+    print(i)
+
+check = True
+while(check):
+    print(1)
+
